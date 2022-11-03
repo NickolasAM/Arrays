190cd3c1333006acdc93f28d6a9ae6af272ccd4e
Nickolas Marquez <nickomarquez7@gmail.com>
2022-11-02 22:58:55 -0600

    Array method

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,15 @@
+# the pop method removed the last element "honda" from the array
 cars = ["ferrari", "porsche", "chevy", "honda"]
+cars.pop
+
+#the pop method removed the last element "32" from the array
 ages = ["21", "25", "30", "32"]
-temperatures = ["32.2", "40.5", "52.3", "65.7"
+ages.pop
+
+# the pop method removed the last element "65.7" from the array
+temperatures = ["32.2", "40.5", "52.3", "65.7"]
+temperatures.pop
+
+# the pop method removed the last element "angry" from the array
 emotions = ["happy", "sad", "mad", "angry"]
+emotions.pop
